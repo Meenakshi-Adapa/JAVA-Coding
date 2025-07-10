1adc5aefc8890fe28178600f1a22e8e830c0632a
CraftHub <crafthub@example.com>
2025-07-10 21:16:16 +0530

    fix class name typo from 'Guantlets' to 'Gauntlets' in Gauntlets.java

--- Hive_problems/Gauntlets.java
+++ Hive_problems/Gauntlets.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Guantlets {
+class Gauntlets {
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
@@ -7,10 +7,15 @@ class Guantlets {
     for(int i =0 ;i<n ;i++) {
       arr[i] = sc.nextInt();
     }
-    
+    int p = 0;
     for( int i = 0 ;i < n ; i++) {
-      for(int j = 0;j< n; j ++)
-
-    }
+      for(int j = 0;j< n; j ++) {
+         if(i!=j && arr[i]==arr[j] && arr[i]!=-1 && arr[j]!=-1){
+            p++;
+            arr[i]=-1;
+            arr[j]=-1;
+         }
+      } 
+    } System.out.println(p);
   }
 }
